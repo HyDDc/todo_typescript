ed983cfe4a37a630e4827799474a4bc3c0a35b23
Huy <mrhuydoduc@gmail.com>
2021-12-07 23:15:36 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>What should I do?!</h1>
+      <h2>The Time is running...</h2>
+    </div>
+  );
+}
